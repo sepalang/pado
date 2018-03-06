1ad6319824540573542c754e3d44df4959fd2ea7
AHN HO JUNG <labeldock@me.com>
2018-03-06 21:54:06 +0900

    move strat script

--- test/src-test.js
+++ test/src-test.js
@@ -1,7 +1,7 @@
-import { promise } from './src'
+import { promise } from '../src'
 
 promise(resolve=>resolve()).then(e=>console.log("promise works!"));
 
-import train from './src/pado'
+import train from '../src/pado'
 
 console.log("train",Object.keys(train));
