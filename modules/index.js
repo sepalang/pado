e61c645a219cbc2683234b2dc0f1d976e24b9690
AHN HO JUNG <labeldock@me.com>
2018-04-19 21:00:41 +0900

    fix build issue

--- modules/index.js
+++ modules/index.js
@@ -1,16 +1,16 @@
 (function (global, factory) {
   if (typeof define === "function" && define.amd) {
-    define(["exports", "./block", "./editable", "./makeup", "./paginate", "./promise", "./timeline"], factory);
+    define(["exports", "./block", "./editable", "./makeup", "./paginate", "./promise", "./timeline", "./operate"], factory);
   } else if (typeof exports !== "undefined") {
-    factory(exports, require("./block"), require("./editable"), require("./makeup"), require("./paginate"), require("./promise"), require("./timeline"));
+    factory(exports, require("./block"), require("./editable"), require("./makeup"), require("./paginate"), require("./promise"), require("./timeline"), require("./operate"));
   } else {
     var mod = {
       exports: {}
     };
-    factory(mod.exports, global.block, global.editable, global.makeup, global.paginate, global.promise, global.timeline);
+    factory(mod.exports, global.block, global.editable, global.makeup, global.paginate, global.promise, global.timeline, global.operate);
     global.index = mod.exports;
   }
-})(this, function (_exports, _block, _editable, _makeup, _paginate, _promise, _timeline) {
+})(this, function (_exports, _block, _editable, _makeup, _paginate, _promise, _timeline, _operate) {
   "use strict";
 
   Object.defineProperty(_exports, "__esModule", {
@@ -72,6 +72,16 @@
       enumerable: true,
       get: function get() {
         return _timeline[key];
+      }
+    });
+  });
+  Object.keys(_operate).forEach(function (key) {
+    if (key === "default" || key === "__esModule") return;
+    if (Object.prototype.hasOwnProperty.call(_exportNames, key)) return;
+    Object.defineProperty(_exports, key, {
+      enumerable: true,
+      get: function get() {
+        return _operate[key];
       }
     });
   });
