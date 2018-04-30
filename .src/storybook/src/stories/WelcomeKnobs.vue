cce62776c6d075126a68fabeffe58814a6efe5aa
AHN HO JUNG <labeldock@me.com>
2018-04-30 14:28:44 +0900

    update storybook for knobs

---
+++ .src/storybook/src/stories/WelcomeKnobs.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main">
+    <h1>Knobs property</h1>
+    <pre>{{ JSON.stringify(knobs,2,2) }}</pre>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ["knobs"]
+  }
+</script>
+
+<style>
+
+</style>
--- .src/storybook/src/stories/index.js
+++ .src/storybook/src/stories/index.js
@@ -11,10 +11,10 @@ story.add('Welcome', () => ({
 
 import { addParameters } from '@storybook/vue';
 import { withNotes } from '@storybook/addon-notes';
-import EditGuide from './EditGuide.vue';
+import WelcomeEdit from './WelcomeEdit.vue';
 story.addDecorator(withNotes)
 story.add('Edit guide', () => ({
-  render: h => h(EditGuide,{ props: { clickLink:action('clickLinkClick') }}),
+  render: h => h(WelcomeEdit,{ props: { clickLink:action('clickLinkClick') }}),
   mounted (){
     this.$children.forEach(c=>{
       c.$on("clickLinkExample",function(e){
@@ -27,3 +27,65 @@ story.add('Edit guide', () => ({
   여기다 <s>Text</s> HTML 메모를 씁니다. 
   `
 });
+
+
+import {
+  withKnobs,
+  text,
+  number,
+  boolean,
+  array,
+  select,
+  color,
+  date,
+  button,
+} from '@storybook/addon-knobs/vue';
+import WelcomeKnobs from './WelcomeKnobs.vue';
+story
+.addDecorator(withKnobs)
+.add('Knobs guide', ()=>{
+  
+  const name = text('Name', 'John Doe');
+  const age = number('Age', 44);
+  const content = `I am ${name} and I'm ${age} years old.`;
+  
+  const stock = number('Stock', 20, {
+    range: true,
+    min: 0,
+    max: 30,
+    step: 5,
+  });
+  
+  const fruits = {
+    apples: 'Apple',
+    bananas: 'Banana',
+    cherries: 'Cherry',
+  };
+  
+  const fruit = select('Fruit', fruits, 'apple');
+  const price = number('Price', 2.25);
+  const colour = color('Border', 'deeppink');
+  const today = date('Today', new Date('Jan 20 2017 GMT+0'));
+  const items = array('Items', ['Laptop', 'Book', 'Whiskey']);
+  const nice = boolean('Nice', true);
+  
+  
+  return {
+    render: h => h(WelcomeKnobs,{
+      props:{
+        knobs:{
+          name,
+          age,
+          content,
+          stock,
+          fruit,
+          price,
+          colour,
+          today,
+          items,
+          nice
+        }
+      }
+    })
+  }
+})
