0dccdd5a6faa07abab2aae7b5dc79484f3a436ef
AHN HO JUNG <labeldock@me.com>
2018-05-10 12:06:15 +0900

    setup basic test environment

--- .src/test/src-test.js
+++
@@ -1,7 +0,0 @@
-import { promise } from '../src'
-
-promise(resolve=>resolve()).then(e=>console.log("promise works!"));
-
-import train from '../src/pado'
-
-console.log("train",Object.keys(train));
