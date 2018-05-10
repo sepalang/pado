06227d1e431c475529fc9ed0c82d3268f3d895f7
AHN HO JUNG <labeldock@me.com>
2018-05-10 12:17:58 +0900

    remove devel menu

--- .src/storybook/src/stories/Welcome.vue
+++ .src/storybook/src/stories/Welcome.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="main">
     <h1>Pado storybook</h1>
-    <p>
+    <dl>
+      <dt>GIT</dt>
+      <dd><a href="https://github.com/sepalang/pado" target="_blank">sepalang/pado</a></dd>
       
-    </p>
+      <dt>Feature</dt>
+      <dd>Functions</dd>
+      <dd>Modules</dd>
+      <dd>Hacks</dd>
+      
+      <dt>Advence</dt>
+      <dd>How use to visual programming</dd>
+      
+    </dl>
   </div>
 </template>
 
@@ -16,3 +26,11 @@
     }
   }
 </script>
+<style>
+  dd+dt {
+    margin-top:20px;
+  }
+  dd {
+    margin-left:20px;
+  }
+</style>
