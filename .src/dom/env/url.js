71fa60bf2f78ada8216532b9abffa2d43121d3e2
AHN HO JUNG <labeldock@me.com>
2018-06-08 17:14:25 +0900

    update dom url

---
+++ .src/dom/env/url.js
@@ -0,0 +1,94 @@
+import { isArray, asArray } from '../../functions';
+
+export const readUrl = function(inputUrl){
+  let info;
+  let url; 
+    
+  try {
+    url  = inputUrl?inputUrl:window.document.URL.toString();
+    info = /([\w]+)(\:[\/]+)([^/]*\@|)([\w\d\.\-\_\+]+)(\:[\d]+|)(\/|)([\w\d\.\/\-\_\;\=]+|)(\?[\d\w\=\&\%\,\.\/\(\)-]+|)(\#[\d\w]*|)/.exec(url);
+  } catch(e) {
+    info = null;
+  }
+  
+  if(info === null) {
+    console.error("faild parse url",e);
+    return {
+      url:url || null,
+      valid:false
+    };
+  }
+  
+  const protocol = info[1];
+  const divider = info[2];
+  const userinfo = info[3];
+  const hostname = info[4];
+  const port = info[5].substring(1);
+  const path = info[6]+info[7];
+  const query = info[8];
+  const fragment = info[9];
+  const filename = /(\/|)([\w\d\.\-\_]+|)$/.exec(info[6]+info[7])[2];
+  const host = info[1]+info[2]+info[4]+info[5];
+  const params = (function(){
+    const result = {};
+    if(query){
+      query.substr(1).split("&").forEach((onePiece,i)=>{
+        const entry = onePiece.split("=");
+        result[decodeURI(entry[0])] = decodeURI(entry[1]);
+      });
+    }
+    return result;
+  }());
+
+  return {
+    url,
+    protocol,
+    divider,
+    userinfo,
+    hostname,
+    port,
+    path,
+    query,
+    fragment,
+    filename,
+    host,
+    params,
+    valid:true
+  };
+};
+    
+export const serialize = function(obj,transform){
+  const params = [];
+  let invalid  = [];
+  
+  Object.keys(obj).forEach(key=>{
+    let value = obj[key];
+    let stringValue = "";
+
+    if(typeof value === "undefined"){
+      return;
+    } else if(value === null){
+      stringValue = "";
+    } else if(isArray(value)){
+      return value.each((val)=>{
+        typeof transform === "function" ?
+        params.push(transform(key) + "=" + transform(val)):
+        params.push(key + "=" + val);
+      });
+    } else if(typeof value === "object") {
+      return invalid.push(key);
+    } else {
+      stringValue = value + "";
+    }
+          
+    typeof transform === "function" ?
+    params.push(transform(key) + "=" + transform(stringValue)):
+    params.push(key + "=" + stringValue);
+  })
+      
+  if(invalid.length){
+    invalid = null;
+  }
+  
+  return params.join("&");
+}
---
+++ .util/test/dom/url.spec.js
@@ -0,0 +1,51 @@
+import { readUrl, serialize } from '../../../.src/dom/env/url';
+
+describe('Dom::env url', () => {
+  it("readUrl - base", ()=>{
+    expect(readUrl("http://nineten11.net")).toEqual({
+      divider: "://", 
+      filename: "", 
+      fragment: "", 
+      host: "http://nineten11.net", 
+      hostname: "nineten11.net", 
+      params: {}, 
+      path: "", 
+      port: "", 
+      protocol: "http", 
+      query: "", 
+      url: "http://nineten11.net", 
+      userinfo: "",
+      valid:true
+    });
+  });
+  
+  it("readUrl - parseParam", ()=>{
+    expect(readUrl("http://nineten11.net?uniqueKey=123&uniqueName=foobar")).toEqual({
+      divider: "://", 
+      filename: "", 
+      fragment: "", 
+      host: "http://nineten11.net", 
+      hostname: "nineten11.net", 
+      params: {
+        uniqueKey:"123",
+        uniqueName:"foobar"
+      }, 
+      path: "", 
+      port: "", 
+      protocol: "http", 
+      query: "?uniqueKey=123&uniqueName=foobar", 
+      url: "http://nineten11.net?uniqueKey=123&uniqueName=foobar", 
+      userinfo: "",
+      valid:true
+    });
+  });
+  
+  it("readUrl", ()=>{
+    expect( serialize({foo:"bar"}) ).toEqual("foo=bar");
+    expect( serialize({foo:"bar",kim:11}) ).toEqual("foo=bar&kim=11");
+    expect( serialize({foo:"bar",kim:undefined}) ).toEqual("foo=bar");
+    expect( serialize({foo:"bar",kim:null}) ).toEqual("foo=bar&kim=");
+  });
+  
+});
+
