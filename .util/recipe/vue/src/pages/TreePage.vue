2f310ec0192cbd142aea3a6a8dd54c7b906e3784
AHN HO JUNG <labeldock@me.com>
2018-08-06 09:49:02 +0900

    update sample (remove Nodes component)

--- .util/recipe/vue/src/pages/TreePage.vue
+++ .util/recipe/vue/src/pages/TreePage.vue
@@ -34,62 +34,16 @@
     </ul>
     <hr>
     
-    <h2>Custom tree</h2>
-    <pre>{{ treeModel }}</pre>
-    <PadoNodes :model="treeModel">
-      <template slot-scope="node">
-        <div>
-          <button 
-            @click="node.toggleOpen" 
-            :disabled="!node.model.children || !node.model.children.length"
-          >OPEN</button>
-          [{{ node.depth }}]
-          {{ node.model.name }}
-        </div>
-        <div v-if="node.open">
-          <PadoNodes :model="node.model.children">
-            <template slot-scope="node">
-              <button
-                v-if="node.model.children && node.model.children.length"
-                @click="node.toggleOpen" 
-              >
-                MORE OPEN
-              </button>
-              [{{ node.depth }}]
-              {{ node.model.name }}
-              <div v-if="node.open">
-                [{{ node.depth + 1 }}]
-                {{ node.model.children }}
-              </div>
-            </template>
-          </PadoNodes>
-        </div>
-      </template>
-    </PadoNodes>
-    
-    <hr>
-    
-    <h2>Auto tree : incomplete</h2>
-    <PadoNodes :model="treeModel" :nested="true">
-      <template slot-scope="node">
-        <button 
-          @click="node.toggleOpen" 
-          :disabled="!node.model.children"
-        > Open: {{ node.open }}</button>
-      </template>
-    </PadoNodes>
   </AppLayout>
 </template>
 <script>
 import AppLayout from '../layouts/AppLayout.vue';
 import Nodelist from '../components/Nodelist.vue';
-import PadoNodes from '../components/PadoNodes.vue';
 
 export default {
   components: {
     AppLayout,
-    Nodelist,
-    PadoNodes
+    Nodelist
   },
   data: ()=>({
     treeModel: [
