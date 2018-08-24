8abc290c055cc293053978b15866fc51372d7288
AHN HO JUNG <labeldock@me.com>
2018-08-24 14:14:59 +0900

    update

--- .util/fnpack/case-b/dataUtil.js
+++ .util/fnpack/case-b/dataUtil.js
@@ -1,5 +1,7 @@
-export { asArray, removeValue } from '../../../.src/functions/cast';
+export { alloc, asArray, removeValue } from '../../../.src/functions/cast';
 export { limitNumber, toggle } from '../../../.src/functions/nice';
 export { isPresence, likeNumber, isEqual, likeEqual } from '../../../.src/functions/isLike';
 export { top, cut, cuts } from '../../../.src/functions/reduce';
 export { fill } from '../../../.src/functions/reform';
+export { domainRangeValue } from '../../../.src/functions/matrix';
+export { times } from '../../../.src/functions/enumerable';
