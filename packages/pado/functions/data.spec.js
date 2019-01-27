fbaabf51fc0abf04180955a6149037b764181029
AHN HO JUNG <labeldock@me.com>
2019-01-28 01:01:35 +0900

    fix bugs

--- packages/pado/functions/data.spec.js
+++ packages/pado/functions/data.spec.js
@@ -9,8 +9,8 @@ export const {
 typeof describe === "function" && describe('Functions data', ()=>{
   
   it('zip', ()=>{
-    expect(zip(['foo','bar'],[1,2])).toEqual({ foo:1, bar:2 })
-    expect(zip([1,2,3],[10,20,30])).toEqual({ "1":10, "2":20, "3":30 })
+    expect(zip(['foo', 'bar'], [1, 2])).toEqual({ foo: 1, bar: 2 })
+    expect(zip([1, 2, 3], [10, 20, 30])).toEqual({ "1": 10, "2": 20, "3": 30 })
   })
   
   //it('zips', ()=>{
