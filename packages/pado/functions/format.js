98b039fbb7ee164acc5f326faa5719a44e283c89
AHN HO JUNG <nb90407@navercorp.com>
2019-04-08 17:35:24 +0900

    add humanReadableBytesValue

--- packages/pado/functions/format.js
+++ packages/pado/functions/format.js
@@ -5,3 +5,28 @@ export const humanReadableBytes = (bytes)=>{
   var pre = "KMGTPE".charAt(exp - 1);
   return (bytes / Math.pow(unit, exp)).toFixed(2) + ' ' + pre + 'B';
 }
+
+export const humanReadableBytesValue = (byteString)=>{
+  if(typeof byteString === "number"){
+    return byteString;
+  }
+  
+  if(/^(|\.|[0-9]+\.)[0-9]+$/.test(byteString)){
+    return Math.ceil(Number(byteString));
+  }
+  
+  const parse = /((|\.|[0-9]+\.)[0-9]+)[\s]*(|K|M|G|T|P|E)B/i.exec(byteString);
+  if(!parse) return null;
+  
+  const number = parse[1];
+  const unit = parse[3];
+  if(number == 0) return 0;
+  if(unit === "") return Number(number);
+  const unitIndex = "KMGTPE".indexOf(unit.toUpperCase());
+  
+  if(unitIndex >= 0){
+    return Math.ceil(number * Math.pow(1024, unitIndex+1));
+  }
+  
+  return null;
+};
